000840ac8e5ee568b999705908b3efea5948645d
Oleksandr Bystrikov <coder@yakviter.com.ua>
2015-12-03 17:03:37 +0100

    Stage UI

--- build/templates-app.js
+++ build/templates-app.js
@@ -2,47 +2,68 @@ angular.module('templates-app', ['stage/stage.tpl.html']);
 
 angular.module("stage/stage.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("stage/stage.tpl.html",
-    "<div class=\"\">\n" +
-    "    \n" +
-    "    <h1>OneFootball Stage Visualizer</h1>\n" +
-    "    <button class=\"btn btn-success btn-default\" ng-click=\"stage.download('/assets/data/brackets.json')\">\n" +
-    "        Start\n" +
-    "    </button>\n" +
+    "<div class=\"knockout\">\n" +
+    "    <div class=\"row round\">\n" +
+    "        <div class=\"col-md-12\">\n" +
+    "            <h1>OneFootball Stage Visualizer</h1>\n" +
+    "            <button class=\"btn btn-success btn-default\" ng-click=\"stage.download('/assets/data/brackets.json')\">\n" +
+    "                Start\n" +
+    "            </button>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
     "    <div class=\"row round no-mobile\">\n" +
-    "        <div class=\"col-md-12\" ng-repeat=\"(count, octave) in octaves\" ng-if=\"count < 4\">\n" +
-    "            {{ octave.teamHome }} vs {{ octave.teamAway }}\n" +
+    "        <div class=\"col-md-3\" ng-repeat=\"(count, octave) in octaves\" ng-if=\"count < 4\">\n" +
+    "            <div class=\"game\">{{ octave.teamHome }} - {{ octave.teamAway }}</div>\n" +
     "        </div>\n" +
     "    </div>\n" +
     "    <div class=\"row round\">\n" +
-    "        <div class=\"col-md-12\" ng-repeat=\"(count, quarter) in quarters\" ng-if=\"count < 2\">\n" +
-    "            {{ quarter.teamHome }} vs {{ quarter.teamAway }}\n" +
+    "        <div class=\"col-md-6\" ng-repeat=\"(count, quarter) in quarters\" ng-if=\"count < 2\">\n" +
+    "            <div class=\"row\">\n" +
+    "                <div class=\"col-md-6 col-md-offset-3\">\n" +
+    "                    <div class=\"game\">{{ quarter.teamHome }} - {{ quarter.teamAway }}</div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
     "        </div>\n" +
     "    </div>\n" +
     "    <div class=\"row round\">\n" +
-    "        <div class=\"col-md-12\" ng-repeat=\"(count, semiFinal) in semiFinals\" ng-if=\"count < 1\">\n" +
-    "            {{ semiFinal.teamHome }} vs {{ semiFinal.teamAway }}\n" +
-    "        </div>\n" +
-    "    </div>\n" +
-    "\n" +
-    "    <div class=\"row round\">\n" +
-    "        <div class=\"col-md-4 col-md-offset-4\" ng-repeat=\"fin in final\">\n" +
-    "            {{ fin.teamHome }} vs {{ fin.teamAway }}\n" +
-    "        </div>\n" +
-    "    </div>\n" +
-    "\n" +
-    "    <div class=\"row round\">\n" +
-    "        <div class=\"col-md-12\" ng-repeat=\"(count, semiFinal) in semiFinals\" ng-if=\"count >= 1\">\n" +
-    "            {{ semiFinal.teamHome }} vs {{ semiFinal.teamAway }}\n" +
+    "        <div class=\"col-md-6 col-md-offset-3\" ng-repeat=\"(count, semiFinal) in semiFinals\" ng-if=\"count < 1\">\n" +
+    "            <div class=\"row\">\n" +
+    "                <div class=\"col-md-6 col-md-offset-3\">\n" +
+    "                    <div class=\"game\">{{ semiFinal.teamHome }} - {{ semiFinal.teamAway }}</div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
     "        </div>\n" +
     "    </div>\n" +
     "    <div class=\"row round\">\n" +
-    "        <div class=\"col-md-12\" ng-repeat=\"(count, quarter) in quarters\" ng-if=\"count >= 2\">\n" +
-    "            {{ quarter.teamHome }} vs {{ quarter.teamAway }}\n" +
+    "        <div class=\"col-md-6 col-md-offset-3\" ng-repeat=\"fin in final\">\n" +
+    "            <div class=\"row\">\n" +
+    "                <div class=\"col-md-6 col-md-offset-3\">\n" +
+    "                    <div class=\"game\">{{ fin.teamHome }} - {{ fin.teamAway }}</div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
     "        </div>\n" +
     "    </div>\n" +
     "    <div class=\"row round\">\n" +
-    "        <div class=\"col-md-12\" ng-repeat=\"(count, octave) in octaves\" ng-if=\"count >= 4\">\n" +
-    "            {{ octave.teamHome }} vs {{ octave.teamAway }}\n" +
+    "        <div class=\"col-md-6 col-md-offset-3\" ng-repeat=\"(count, semiFinal) in semiFinals\" ng-if=\"count >= 1\">\n" +
+    "            <div class=\"row\">\n" +
+    "                <div class=\"col-md-6 col-md-offset-3\">\n" +
+    "                    <div class=\"game\">{{ semiFinal.teamHome }} - {{ semiFinal.teamAway }}</div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "    <div class=\"row round\">\n" +
+    "        <div class=\"col-md-6\" ng-repeat=\"(count, quarter) in quarters\" ng-if=\"count >= 2\">\n" +
+    "            <div class=\"row\">\n" +
+    "                <div class=\"col-md-6 col-md-offset-3\">\n" +
+    "                    <div class=\"game\">{{ quarter.teamHome }} - {{ quarter.teamAway }}</div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "    </div>\n" +
+    "    <div class=\"row round no-mobile\">\n" +
+    "        <div class=\"col-md-3\" ng-repeat=\"(count, octave) in octaves\" ng-if=\"count >= 4\">\n" +
+    "            <div class=\"game\">{{ octave.teamHome }} - {{ octave.teamAway }}</div>\n" +
     "        </div>\n" +
     "    </div>\n" +
     "</div>\n" +
